22109e53cdf24e7dff50fbb57864134477cb86c9
Zoneam <zoneam@gmail.com>
2022-07-14 16:59:35 -0700

    changing name on basic package

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Login from "./pages/Login";
 import StripeSuccess from "./pages/stripe-success";
 import StripeCancel from "./pages/stripe-cancel";
 import Account from "./pages/Account";
-import Basic from "./pages/plans/Basic";
+import Premium from "./pages/plans/Premium";
 
 function App() {
   return (
@@ -28,7 +28,7 @@ function App() {
         <AuthRoute exact path="/stripe/success" component={StripeSuccess} />
         <AuthRoute exact path="/stripe/cancel" component={StripeCancel} />
         <AuthRoute exact path="/account" component={Account} />
-        <AuthRoute exact path="/premium" component={Basic} />
+        <AuthRoute exact path="/premium" component={Premium} />
       </Switch>
     </Router>
   );
